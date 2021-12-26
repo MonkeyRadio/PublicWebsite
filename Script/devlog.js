e0bc9dea31ec620b56f52f63f2cbfc55cd79bf7e
Nicolas Jacquemin <jacq59nico@gmail.com>
2021-12-26 12:17:31 +0100

    Fix - devlog

--- Script/devlog.js
+++ Script/devlog.js
@@ -1,7 +1,7 @@
 const devlog = new bootstrap.Modal(document.querySelector(".devLogger"));
 if(window.location.hostname=="www.dev.monkeyradio.fr"){
 window.onerror = function(message, url, lineNumber) {  
-    document.querySelector(".devloggerdiv").innerHTML="<h6>Erreur JS Ligne "+lineNumber+"</h6><br/><p>"+message+"</p>";
+    document.querySelector(".devloggerdiv").innerHTML="<h6>Erreur JS "+url+" Ligne "+lineNumber+"</h6><br/><p>"+message+"</p>";
     devlog.toggle();
     return true;
   };  
