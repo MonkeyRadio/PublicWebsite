a6467d09e21c5b0ab54701c1aaf80fdb4a730d7e
Nicolas Jacquemin <jacq59nico@gmail.com>
2021-12-30 18:56:43 +0100

    Feat - add eruda

--- Script/devlog.js
+++ Script/devlog.js
@@ -1,17 +1,19 @@
 const devlog = new bootstrap.Modal(document.querySelector(".devLogger"));
 const devlogz = new bootstrap.Modal(document.querySelector(".Logger"));
-if(window.location.hostname=="www.dev.monkeyradio.fr"){
-window.onerror = function(message, url, lineNumber) {  
-    document.querySelector(".devloggerdiv").innerHTML="<h6>Erreur JS "+url+" Ligne "+lineNumber+"</h6><br/><p>"+message+"</p>";
-    devlog.toggle();
-    return true;
-  };  
+if (window.location.hostname == "www.dev.monkeyradio.fr") {
+    window.onerror = function(message, url, lineNumber) {
+        document.querySelector(".devloggerdiv").innerHTML = "<h6>Erreur JS " + url + " Ligne " + lineNumber + "</h6><br/><p>" + message + "</p>";
+        devlog.toggle();
+        return true;
+    };
+
+    eruda.init();
 
 }
 
-function log(a){
-    if(window.location.hostname=="www.dev.monkeyradio.fr"){
-    document.querySelector(".devlogdiv").innerHTML+="<p>"+a+"</p>";
-    devlogz.toggle();
+function log(a) {
+    if (window.location.hostname == "www.dev.monkeyradio.fr") {
+        document.querySelector(".devlogdiv").innerHTML += "<p>" + a + "</p>";
+        devlogz.toggle();
     }
 }
