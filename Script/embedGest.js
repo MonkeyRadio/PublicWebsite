23b264d387df3ad6f31403ac196b9be7e827ed61
Nicolas Jacquemin <jacq59nico@gmail.com>
2022-01-11 12:59:14 +0100

    Feat - New design init

--- Script/embedGest.js
+++ Script/embedGest.js
@@ -2,13 +2,17 @@ addEventListener("load", () => {
     styleAll(".embed", "none")
     document.querySelector("#playerbdiv").style.display = "block";
 
-    var elems = document.querySelectorAll(".segbutton");
+    var elems = document.querySelectorAll(".navitem");
     var index = 0,
         length = elems.length;
     for (; index < length; index++) {
         elems[index].addEventListener("click", (e) => {
             styleAll(".embed", "none")
+            try{
             document.querySelector("#" + e.target.id + "div").style.display = "block";
+            }catch(a){
+                console.log(e)
+            }
             if (e.target.id == "playlistb") document.querySelector('#playlistbdiv').scrollTop = document.querySelector('.timeline-now').offsetTop - 300;
             if (e.target.id == "playerb") {
                 bplayer(false)
--- Script/playerStyle.js
+++ Script/playerStyle.js
@@ -1,6 +1,6 @@
 function bplayer(a){
     if(a==true){
-      document.querySelector(".bottomplayer").style.bottom="0px";
+      document.querySelector(".bottomplayer").style.bottom="50px";
       document.querySelector(".player-bfix").style.display="block";
     }else{
       document.querySelector(".bottomplayer").style.bottom="-110px";
