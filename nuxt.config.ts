b3caa4a307ecdd2714f67996c1a5586b9dc069e3
Nicolas <jacquemin.nicolas59@gmail.com>
2023-08-05 15:55:42 +0200

    [+] Set real favicon pack + metatags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,8 @@ const title = "Monkey";
 const shortTitle = "Monkey";
 const description =
   "Ici c'est Monkey ! Ecoutez et partagez vos musiques préférées sur votre radio...";
-const image = "";
-const url = "";
+const image = "https://monkeyradio.fr/large-icon.png";
+const url = "https://monkeyradio.fr";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -43,9 +43,48 @@ export default defineNuxtConfig({
         { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
         { rel: "preconnect", href: "https://rsms.me/" },
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#9d5762" },
         { rel: "canonical", href: url },
       ],
       meta: [
+        { name: "apple-mobile-web-app-title", content: "MonkeyRadio" },
+        { name: "application-name", content: "MonkeyRadio" },
+        { name: "msapplication-TileColor", content: "#9d5762" },
+        { name: "theme-color", content: "#9d5762" },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          hid: "twitter:url",
+          name: "twitter:url",
+          content: url,
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: title,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: description,
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: image,
+        },
+        {
+          hid: "title",
+          name: "title",
+          content: title,
+        },
         {
           hid: "description",
           name: "description",
@@ -77,7 +116,7 @@ export default defineNuxtConfig({
           hid: "og:image",
           property: "og:image",
           content: image,
-        }
+        },
       ],
     },
   },
